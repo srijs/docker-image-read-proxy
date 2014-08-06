54e815ad94012004214c32d980c3be85fd1c26e4
Sam Reis <sreis@atlassian.com>
2014-08-07 01:05:39 +1000

    use tmp files

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var http  = require('http'),
     https = require('https'),
-    url   = require('url'),
-    fs    = require('fs'),
-    path  = require('path');
+    path  = require('path'),
+    fs    = require('fs');
 
 var config = require('./config');
 
 var server = http.createServer(function (request, response) {
 
   var method   = request.method,
-      pathname = url.parse(request.url).pathname,
-      headers  = request.headers;
+      pathname = request.url,
+      headers  = request.headers,
+      length   = parseInt(headers['content-length'], 10);
 
   if (config.https.auth)  {
     delete headers['authorization'];
@@ -36,71 +36,77 @@ var server = http.createServer(function (request, response) {
     options[key] = config.https[key];
   });
 
-  var file = '', match;
+  var key = '', temp, file = '', match;
   if (match = request.url.match(/^\/v1\/images\/([a-f0-9]{64})\/([a-z]+)/)) {
-    file = path.join(config.local.path, match[1] + '-' + match[2]);
+    key  = match[1] + '-' + match[2];
+    temp = path.join('/tmp', key + '-' + Date.now());
+    file = path.join(config.local.path, key);
   }
 
   fs.stat(file, function (err, stat) {
     
-    if (err) {
+    if (!err) {
+      response.writeHead(200);
+      fs.createReadStream(file).pipe(response);
+      return;
+    }
+
+    console.error('MISS ' + request.url);
     
-      console.error('MISS ' + request.url);
-      
-      var backendRequest = https.request(options, function (backendResponse) {
+    var backendRequest = https.request(options, function (backendResponse) {
 
-        var fileWriteStream;
+      var fileWriteStream, bytesWritten = 0;
 
-        backendResponse.on('error', function (err) {
-          if (fileWriteStream) {
-            fs.unlink(file, function (err) {
-              console.error(err);
+      console.log(backendResponse.headers);
+
+      response.writeHead(backendResponse.statusCode, backendResponse.headers);
+
+      backendResponse.on('data', function (data) {
+        bytesWritten += data.length;
+        if (!response.write(data)) {
+          backendResponse.pause();
+        }
+        if (!fileWriteStream && file && backendResponse.statusCode === 200) {
+          fileWriteStream = fs.createWriteStream(temp);
+        }
+        if (fileWriteStream && !fileWriteStream.write(data)) {
+          backendResponse.pause();
+        }
+      });
+
+      backendResponse.on('error', function (err) {
+        console.error(err);
+      });
+
+      backendResponse.once('end', function () {
+        response.end();
+        if (fileWriteStream) {
+          fileWriteStream.end();
+          if (bytesWritten < length) {
+            fs.unlink(temp, function (err) {
+              if (err) {
+                console.error(err);
+              }
+            });
+          } else {
+            fs.rename(temp, file, function (err) {
+              if (err) {
+                console.error(err);
+              }
             });
           }
-          console.error(err);
-        });
-
-        response.writeHead(backendResponse.statusCode, backendResponse.headers);
-
-        backendResponse.on('data', function (data) {
-          if (!response.write(data)) {
-            backendResponse.pause();
-          }
-          if (!fileWriteStream && file && backendResponse.statusCode === 200) {
-            fileWriteStream = fs.createWriteStream(file);
-          }
-          if (fileWriteStream) {
-            if (!fileWriteStream.write(data)) {
-              backendResponse.pause();
-            }
-          }
-        });
-
-        backendResponse.on('end', function () {
-          response.end();
-          if (fileWriteStream) {
-            fileWriteStream.end();
-          }
-        });
-
+        }
       });
 
-      backendRequest.on('error', function (error) {
-        response.writeHead(500);
-        console.error(error);
-        response.end();
-      });
+    });
 
-      backendRequest.end();
+    backendRequest.on('error', function (error) {
+      response.writeHead(500);
+      console.error(error);
+      response.end();
+    });
 
-    } else {
-
-      var fileReadStream = fs.createReadStream(file);
-      console.error('HIT  ' + request.url);
-      response.writeHead(200);
-      fileReadStream.pipe(response);
-
-    }
+    backendRequest.end();
 
   });
 
